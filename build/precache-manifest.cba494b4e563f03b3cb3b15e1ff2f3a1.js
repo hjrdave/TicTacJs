b4266c063025aae9daee668c4e3c624e548960e5
hunterjr88 <dsanders_m633@outlook.com>
2018-11-06 15:42:24 -0500

    made production ready server and files.

---
+++ build/precache-manifest.cba494b4e563f03b3cb3b15e1ff2f3a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d7c093c9c40efc7c2dfa",
+    "url": "/static/js/main.d7c093c9.chunk.js"
+  },
+  {
+    "revision": "8391c07e1621390095e6",
+    "url": "/static/js/1.8391c07e.chunk.js"
+  },
+  {
+    "revision": "d7c093c9c40efc7c2dfa",
+    "url": "/static/css/main.5967bfde.chunk.css"
+  },
+  {
+    "revision": "f284b38c23ec8abc0a184af57610880b",
+    "url": "/index.html"
+  }
+];
